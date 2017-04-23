d77ea22604d7a830e7e4b489bc3ac402abfa7d9e
Matt Larson <matt.a.larson@gmail.com>
2017-04-23 11:55:24 -0500

    working on functionality

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-// client side will take in the values (in 2 input text fields) and the type of
+// client side will take in the values and the type of
 // mathematical operation (selected using a button on the DOM).
 // Convert the input fields for the two values to Buttons. So the experience
 // would allow the user to click on a numerical button, then a mathematical
@@ -6,33 +6,76 @@
 // sends all of the information to the server.
 // Finally, build a 'clear' button that resets the whole experience.
 
-
-
-$(document).ready(onReady);
-console.log('JQ');
+$( document ).ready( onReady );
+console.log( 'JQ' );
 
 var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 var operators = ['+', '-', '*', '/', '='];
-var result = '';
+var total = '';
 
 //function with event listeners that runs when doc loads
 function onReady(){
-  console.log('onReady');
-  $('#clear').on('click', clearFunc);
-  $('#plus').on('click', buttonFunc);
-  $('#minus').on('click', buttonFunc);
-  $('#times').on('click', buttonFunc);
-  $('#divides').on('click', buttonFunc);
-  $('#equals').on('click', buttonFunc);
-  $('.numbers').on('click', buttonFunc);
+  console.log( 'onReady' );
+  //operator buttons
+  $( '#clear' ).on( 'click', clearFunc );
+  $( '#plus' ).on( 'click', buttonFunc );
+  $( '#minus' ).on( 'click', buttonFunc );
+  $( '#times' ).on( 'click', buttonFunc );
+  $( '#divides' ).on( 'click', buttonFunc );
+  $( '#equals' ).on( 'click', equals );
+  // number buttons
+  $('#seven').on('click', buttonFunc);
+  $('#eight').on('click', buttonFunc);
+  $('#nine').on('click', buttonFunc);
+  $('#four').on('click', buttonFunc);
+  $('#five').on('click', buttonFunc);
+  $('#six').on('click', buttonFunc);
+  $('#one').on('click', buttonFunc);
+  $('#two').on('click', buttonFunc);
+  $('#three').on('click', buttonFunc);
+  $('#zero').on('click', buttonFunc);
+
 } // end onReady
 
+function equals(){
+  console.log( 'equals' );
+
+  var xNum = '';
+  var yNum = '';
+
+  var objectToSend = {
+    x: xNum,
+    y: yNum,
+    // operators:
+  };
+
+  // ajax's
+  $.ajax({
+    url: '/calc',
+    method: 'POST',
+    data: objectToSend,
+    success: function( response ){
+      console.log( response );
+    }
+  });
+
+  $.ajax({
+    url: '/calc',
+    method: 'GET',
+    success: function( response ){
+    }
+  });
+} // end equals
 
 function buttonFunc() {
-  console.log('buttonFunc');
+  console.log( 'buttonFunc' );
+  clearFunc();
+  $( "#result" ).text();
+  total += $( this ).text();
+    $( '#result' ).text( total );
 }
 
 function clearFunc() {
-  $('#result').empty();
-  result = '';
+  $( '#result' ).empty();
+  total = '';
 }
--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
 
 //globals
-
+var total = 0;
+var xNum = 0;
+var yNum = 0;
 
 // uses
 app.use( express.static( 'public' ) );
@@ -19,4 +21,26 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 // server at 3000
 app.listen( 3000, function(){
   console.log( 'server up on:', 3000 );
+});
+
+// POST
+app.post( '/calc', function( req,res ){
+  if ( operators === "+" ){
+    total = x + y;
+  }
+  else if ( operators === "-" ){
+    total = x - y;
+  }
+  else if ( operators === "/" ){
+    total = x / y;
+  }
+  else if ( operators === "*" ){
+    total = x * y;
+  }
+  res.status( 200 );
+});
+
+// GET
+app.get( '/calc', function( req,res ){
+  console.log( '/calc' );
 });
